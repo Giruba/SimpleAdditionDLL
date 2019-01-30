2683fb30431491ae5b236328de962ab9b1655da9
Giruba Beulah SE <gibeulah@microsoft.com>
2019-01-30 11:55:09 +0530

    Add project files.

---
+++ GirubaSumDLL/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GirubaSumDLL
+{
+    public class Class1
+    {
+        public int AddTwoNumbers(int a, int b) {
+            return a + b;
+        }
+
+        public int AddThreeNumbers(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        public int AddNumbersInRange(int endNumber) {
+            int sum = 0;
+            for (int i = 0; i <= endNumber; i++) {
+                sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
